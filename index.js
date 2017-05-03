3e5947a33e7b19ae3a861c36444965fd5c5ba24f
Fan Minghui <fmh_33@foxmail.com>
2017-05-03 10:53:43 +0800

    modify

--- index.js
+++ index.js
@@ -30,15 +30,40 @@ var mine = {
     ".xml": "text/xml"
 }; 
 
+var GAME_DATA = {
+    appid : 'h5_testh5_pfBhAtb',
+    app_secret : '1B3B06D1-BF59-D7E5-E9C4-156EE38BBB5C'
+};
+
 var server = http.createServer(function(req, res) {
 	
 	var pathname = url.parse(req.url).pathname;
 
 	if(pathname === '/'){
-		pathname = "index.html";
+		pathname = "view/index.html";
 	}
 
-	pathname = "view/" + pathname;
+    // if(pathname === '/test') {
+
+    //     if(req.method == 'POST') {
+    //         var body = '';
+
+    //         req.on('data', function(data) {
+    //             body += data;
+    //         });
+
+    //         req.on('end', function() {
+    //             console.log(body)
+    //         })
+
+    //     }
+
+    //     return;
+
+    // }else{
+    pathname = "view/" + pathname;
+    // }
+
 
 	fs.exists(pathname, function(exists){ 
         if(exists){
@@ -52,7 +77,8 @@ var server = http.createServer(function(req, res) {
             fs.readFile(pathname,function (err,data){ 
                 res.end(data); 
             }); 
-        } else { 
+
+        }else{ 
             res.writeHead(404, {"Content-Type": "text/html"}); 
             res.end("<h1>404 Not Found</h1>"); 
         } 
@@ -83,10 +109,14 @@ io.on('connection', function(socket) {
 
     socket.on('createClient', function(clientInfo) {
         allSocket[socket.id] = socket;
-
+        allSocket[socket.id].ext = clientInfo.ext;
         waitingClients.push(clientInfo);
 
-        // console.log(waitingClients)
+        updateLoc(allSocket[socket.id].ext);
+
+        if(waitingClients.length === 1) {
+            console.log('waitingClient: ' + clientInfo.name)
+        }
         
         if(waitingClients.length === 2){
             var room = new Room(waitingClients[0], waitingClients[1]);
@@ -106,27 +136,32 @@ io.on('connection', function(socket) {
     });
 
     socket.on('disconnect', function() {
-        console.log('disconnect : ');
-        console.log(socket.id)
+        console.log('disconnect : ' + socket.id);
+
+        if(allSocket[socket.id] && allSocket[socket.id].ext) {
+            updateLoc(allSocket[socket.id].ext, 'del');
+        }
 
         if(waitingClients[0] && waitingClients[0].socketId === socket.id) {
             waitingClients = [];
         }else if(allSocket[socket.id]) {
             var rmRoom = allSocket[socket.id];
-            var OppClientId = rmRoom.getOpponent(socket.id);
-            delete allSocket[socket.id];
-            delete allSocket[OppClientId];
+            if(rmRoom.getOpponent) {
+                var OppClientId = rmRoom.getOpponent(socket.id);
+                delete allSocket[socket.id];
+                delete allSocket[OppClientId];
 
-            allSocket[OppClientId].emit('restart');
+                allSocket[OppClientId].emit('restart');
+            }
+            
             // waitingClients.push()
         }
-
-
 
     });
 
 	
 });
+
 
 function initRandomGrid(rows, columns, randomNum) {
     var randomArr = [];
@@ -166,4 +201,56 @@ Room.prototype.initRandomGrid = function() {
 
 Room.prototype.getOpponent = function(clientId) {
     return this.roomId.replace(clientId, '');
+};
+
+function updateLoc(userinfo, type) {
+
+    var postData = {
+        'appid' : GAME_DATA.appid,
+        'app_secret' : GAME_DATA.app_secret,
+        'data' : type == 'del' ? JSON.stringify([userinfo.userid]) : JSON.stringify([{
+            'gameid' : userinfo.userid,
+            'sex' : userinfo.sex,
+            'lat' : userinfo.lat,
+            'lng' : userinfo.lng
+        }])
+    };
+
+    var querystring = require("querystring");
+    var qs = querystring.stringify(postData);
+    var qslength = qs.length;
+    var options = {
+        hostname: "game-api.immomo.com",
+        port: 80,
+        path: type == 'del' ? '/3/server/location/del-loc' : '/3/server/location/update-loc',
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': qslength
+        }
+    };
+
+    var buffer = "";
+    var req = http.request(options, function(res) {
+        res.on('data', function (chunk) {
+           buffer+=chunk;
+        });
+        res.on('end', function() {
+            console.log(buffer);
+        });
+    });
+
+    req.write(qs);
+    req.end();
+
+}
+
+
+function getParam(query, name) {
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+    var r = query.match(reg);
+    if (r!=null) {
+        return unescape(r[2]);
+    }
+    return null;
 };
